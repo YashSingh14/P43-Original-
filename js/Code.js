b62d78591b94b713ec4b2b37ef56ce18483b0717
YashSingh14 <74043660+YashSingh14@users.noreply.github.com>
2021-06-26 18:50:05 +0545

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "NECKLACE";
+const accessCode3 = "TELEPHONE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("C E A L C K N E", 100,170)
+fill("lightblue")
+text("Hint: Worn by womens !!", 100,190)
+
+fill("white")
+textSize(15)
+text("L T E P E N O H E", 100,270)
+fill("lightblue")
+text("Hint: A mean for communication !!", 100,290)
+
+}
